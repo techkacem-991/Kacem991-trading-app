92fd3b0a1bb18df20714314ad25e1f9015ed8100
techkacem-991 <techkacem@gmail.com>
2026-08-07 00:20:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -171,7 +171,7 @@ HTML_TEMPLATE = """
             transition: opacity 0.2s ease;
         }
         .social-btn:hover { opacity: 0.9; }
-        .btn-facebook { background-color: #1877f2; }
+        .btn-facebook { background: linear-gradient(45deg, #2fbd89, #20b0a2, #13bbd1, #136fd1, #1326d1); }
         .btn-instagram { background: linear-gradient(45deg, #f09433, #e6683c, #dc2743, #cc2366, #bc1888); }
 
         .card { 
